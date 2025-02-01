bfa142389375311f098cc7d1714124a72f92e327
Issak <issakthomas22@gmail.com>
2025-02-01 22:47:22 +0530

    V1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000;
 
 app.use(
 	cors({
